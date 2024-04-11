4ceb0c0beae17a734dee832cea62619a7c918fd3
Haoran Ding <dinghr537@gmail.com>
2024-04-10 21:56:54 -0700

    axios请求注入token

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,4 +1,12 @@
+import { http } from "@/utils";
+import { useEffect } from "react";
+
 function Layout() {
+    useEffect(()=>{
+      http.get('/user/profile')
+    }, [])
+
+
     return (
       <>
         <div className="Layout">
