11b1bbc05b9d3b437187a8d360dc0fcf29187327
Haoran Ding <dinghr537@gmail.com>
2024-04-12 15:13:02 -0700

    basic article publish

--- src/apis/article.js
+++ src/apis/article.js
@@ -3,3 +3,11 @@ import { http } from "@/utils"
 export function getChannelAPI() {
   return http.get("/channels")
 }
+
+export function createArticleAPI(article) {
+  return http({
+    method: 'POST',
+    url: '/mp/articles?draft=false',
+    data: article
+  })
+}
--- src/pages/Publish/index.js
+++ src/pages/Publish/index.js
@@ -15,77 +15,92 @@ import './index.scss'
 import ReactQuill from 'react-quill'
 import 'react-quill/dist/quill.snow.css'
 import { useEffect, useState } from 'react'
-import { getChannelAPI } from '@/apis/article'
+import { getChannelAPI, createArticleAPI } from '@/apis/article'
   
-  const { Option } = Select
-  
-  const Publish = () => {
+const { Option } = Select
 
-    const [channelList, setChannelList] = useState([])
+const Publish = () => {
 
-    useEffect(() => {
-        const getChannelList = async () => {
-            const res = await getChannelAPI()
-            setChannelList(res.data.channels)
+const [channelList, setChannelList] = useState([])
+
+useEffect(() => {
+    const getChannelList = async () => {
+        const res = await getChannelAPI()
+        setChannelList(res.data.channels)
+    }
+    getChannelList()
+}, [])
+
+const onFinish = (formValue) => {
+    const {channel_id, content, title} = formValue
+    const reqData = {
+        title,
+        content,
+        cover: {
+            type: 0,
+            images: []
+        },
+        channel_id
+    }
+    createArticleAPI(reqData)
+}
+return (
+    <div className="publish">
+    <Card
+        title={
+        <Breadcrumb items={[
+            { title: <Link to={'/'}>首页</Link> },
+            { title: '发布文章' },
+        ]}
+        />
         }
-        getChannelList()
-    }, [])
-    return (
-      <div className="publish">
-        <Card
-          title={
-            <Breadcrumb items={[
-              { title: <Link to={'/'}>首页</Link> },
-              { title: '发布文章' },
-            ]}
-            />
-          }
+    >
+        <Form
+        labelCol={{ span: 4 }}
+        wrapperCol={{ span: 16 }}
+        initialValues={{ type: 1 }}
+        onFinish={onFinish}
         >
-          <Form
-            labelCol={{ span: 4 }}
-            wrapperCol={{ span: 16 }}
-            initialValues={{ type: 1 }}
-          >
-            <Form.Item
-              label="标题"
-              name="title"
-              rules={[{ required: true, message: '请输入文章标题' }]}
-            >
-              <Input placeholder="请输入文章标题" style={{ width: 400 }} />
-            </Form.Item>
-            <Form.Item
-              label="频道"
-              name="channel_id"
-              rules={[{ required: true, message: '请选择文章频道' }]}
-            >
-              <Select placeholder="请选择文章频道" style={{ width: 400 }}>
-                {channelList.map(item => <Option key={item.id} value={item.id}>{item.name}</Option>)}
-                
-              </Select>
-            </Form.Item>
-            <Form.Item
-              label="内容"
-              name="content"
-              rules={[{ required: true, message: '请输入文章内容' }]}
-            >
-                <ReactQuill 
-                    className='publish-quill'
-                    theme='snow'
-                    placeholder='请输入文章内容'
-                />
-            </Form.Item>
-  
-            <Form.Item wrapperCol={{ offset: 4 }}>
-              <Space>
-                <Button size="large" type="primary" htmlType="submit">
-                  发布文章
-                </Button>
-              </Space>
-            </Form.Item>
-          </Form>
-        </Card>
-      </div>
-    )
-  }
-  
-  export default Publish
+        <Form.Item
+            label="标题"
+            name="title"
+            rules={[{ required: true, message: '请输入文章标题' }]}
+        >
+            <Input placeholder="请输入文章标题" style={{ width: 400 }} />
+        </Form.Item>
+        <Form.Item
+            label="频道"
+            name="channel_id"
+            rules={[{ required: true, message: '请选择文章频道' }]}
+        >
+            <Select placeholder="请选择文章频道" style={{ width: 400 }}>
+            {channelList.map(item => <Option key={item.id} value={item.id}>{item.name}</Option>)}
+            
+            </Select>
+        </Form.Item>
+        <Form.Item
+            label="内容"
+            name="content"
+            rules={[{ required: true, message: '请输入文章内容' }]}
+        >
+            <ReactQuill 
+                className='publish-quill'
+                theme='snow'
+                placeholder='请输入文章内容'
+            />
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ offset: 4 }}>
+            <Space>
+            <Button size="large" type="primary" htmlType="submit">
+                发布文章
+            </Button>
+            </Space>
+        </Form.Item>
+        </Form>
+    </Card>
+    </div>
+)
+}
+
+export default Publish
