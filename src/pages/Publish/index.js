93d8127dc70ba67bb88e732173fbfcbdb77d1c9b
Haoran Ding <dinghr537@gmail.com>
2024-04-12 22:04:12 -0700

    可为发布文章选择封面

--- src/pages/Publish/index.js
+++ src/pages/Publish/index.js
@@ -7,7 +7,8 @@ import {
     Input,
     Upload,
     Space,
-    Select
+    Select,
+    message
   } from 'antd'
 import { PlusOutlined } from '@ant-design/icons'
 import { Link } from 'react-router-dom'
@@ -34,13 +35,16 @@ useEffect(() => {
 }, [])
 
 const onFinish = (formValue) => {
+    if (imageList.length !== imageType) {
+        return message.warning('请上传正确数量的图片')
+    }
     const {channel_id, content, title} = formValue
     const reqData = {
         title,
         content,
         cover: {
-            type: 0,
-            images: []
+            type: imageType,
+            images: imageList.map(item => item.response.data.url)
         },
         channel_id
     }
